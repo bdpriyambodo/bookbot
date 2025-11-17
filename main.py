1b319d6a6fd49228cef167c0b55a91b32c230530
bdpriyambodo <b.danang.priyambodo@gmail.com>
2025-11-17 16:58:39 +0700

    add character count in stats.py -- CH2 L6

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 filepath = "books/frankenstein.txt"
 
 def main (filepath):
     print(f"Found {get_num_words(filepath)} total words")
+    print(get_num_char(filepath))
 
 main(filepath)
 
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+from enum import unique
+
+
 def get_book_text(filepath):
     # filepath = "/home/bdp/project/learning/workspace/github.com/bdpriyambodo/bookbot/books/frankenstein.txt"
     with open(filepath) as f:
@@ -8,4 +11,23 @@ def get_num_words(filepath):
     text_book = get_book_text(filepath)
     total_word = len(text_book.split())
     return total_word
+
+def get_num_char(filepath):
+    text_book = get_book_text(filepath).lower()
+    chars = list(text_book)
+    unique_chars = sorted(set(chars))
+    char_count = {}
+    for c in unique_chars:
+        char_count[c] = chars.count(c)
+    return char_count
+    # print(f"Total characters: {len(chars)}")
+    # print(f"Total unique characters: {len(unique_chars)}, All: {unique_chars}")
+    # print(char_count)
+
+
+# TEST
+# filepath = "books/frankenstein.txt"
+# get_num_char(filepath)
+
+
 
