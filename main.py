5288999c28d48ef9c5d2edb926895c974ba78f27
bdpriyambodo <b.danang.priyambodo@gmail.com>
2025-11-17 15:31:29 +0700

    update main-py for counting total words -- CH2 L4

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ def get_book_text(filepath):
         file_contents = f.read()
     return str(file_contents)
 
-def main():
+# def main():
+#     filepath = "books/frankenstein.txt"
+#     print(get_book_text(filepath))
+
+def main ():
     filepath = "books/frankenstein.txt"
-    print(get_book_text(filepath))
+    text_book = get_book_text(filepath)
+    total_word = len(text_book.split())
+    print(f"Found {total_word} total words")
 
 main()
 
